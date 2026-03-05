9cce7d32a26d1572b7740b332a113a9b2a8ab1ed
Alex <m.r.alex6684@gmail.com>
2026-03-05 09:25:25 -0500

    Actualizacion4

--- JS/app.js
+++ JS/app.js
@@ -52,3 +52,36 @@ cargarEmpleados()
 
 
 window.onload = cargarEmpleados
+
+async function login(){
+
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+
+const res = await fetch(API + "login", {
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({email,password})
+
+});
+
+const data = await res.json();
+
+if(data.usuario){
+
+localStorage.setItem("usuario",JSON.stringify(data.usuario));
+
+window.location.href = "dashboard.html";
+
+}else{
+
+alert(data.error);
+
+}
+
+}
